44ad3c8356d48d68695891f625415d0e2dffcf31
EZLiang <47573436+EZLiang@users.noreply.github.com>
2020-12-03 15:51:35 -0500

    dvgolue, v1

---
+++ header.js
@@ -0,0 +1,29 @@
+var header = `<div style="text-align: center; color: #0192DA; font-family: Verdana, Geneva, Tahoma, sans-serif; display: flex" width="60em">
+<div class="viewer">
+  <textarea style="display: none">x = 12, y = 12, rule = B3/S23
+    8bo$7bobo$8bo2$6b5o$5bo4bo$4bo2bo$bo2bob2o$obobo5bo$bo2bo4bobo$4b2o2bo
+    2bo$9b2o!
+    #C [[ THUMBNAIL AUTOFIT NOGUI COLOR ALIVE 1 146 218 COLOR BACKGROUND 192 255 238 ]]
+    </textarea>
+  <br>
+  <canvas width="10em" height="10em"></canvas>
+</div>
+<div style="display: flex; align-items: center">
+  <div>
+    <h1 style="margin: 0.2em">Dvgolue</h1>.boatsya.com
+  </div>
+</div>
+</div>
+<div>
+<ul style="text-align: center">
+  <li style="display: inline"><a>Home</a></li>&nbsp;&nbsp;&nbsp;
+  <li style="display: inline"><a>Rules</a></li>&nbsp;&nbsp;&nbsp;
+  <li style="display: inline"><a>Objects</a></li>&nbsp;&nbsp;&nbsp;
+  <li style="display: inline"><a>Census</a></li>&nbsp;&nbsp;&nbsp;
+  <li style="display: inline"><a>Software</a></li>&nbsp;&nbsp;&nbsp;
+  <li style="display: inline"><a>Syntheses</a></li>&nbsp;&nbsp;&nbsp;
+  <li style="display: inline"><a>Statistics</a></li>
+</ul>
+</div>`
+
+document.write(header);

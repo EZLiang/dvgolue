c281b9ab54ec70fadd261b9aeb83196337db60bf
EZLiang <47573436+EZLiang@users.noreply.github.com>
2020-12-03 17:27:48 -0500

    improve colors

--- header.js
+++ header.js
@@ -1,9 +1,9 @@
-var header = `<div style="text-align: center; color: #0192DA; font-family: Verdana, Geneva, Tahoma, sans-serif; display: flex" width="60em">
+var header = `<div style="text-align: center; color: #0888D2; font-family: Verdana, Geneva, Tahoma, sans-serif; display: flex" width="60em">
 <div class="viewer">
   <textarea style="display: none">x = 12, y = 12, rule = B3/S23
     8bo$7bobo$8bo2$6b5o$5bo4bo$4bo2bo$bo2bob2o$obobo5bo$bo2bo4bobo$4b2o2bo
     2bo$9b2o!
-    #C [[ THUMBNAIL AUTOFIT NOGUI COLOR ALIVE 1 146 218 COLOR BACKGROUND 192 255 238 ]]
+    #C [[ THUMBNAIL AUTOFIT NOGUI COLOR ALIVE 8 136 210 COLOR BACKGROUND 192 255 238 ]]
     </textarea>
   <br>
   <canvas width="10em" height="10em"></canvas>
@@ -14,9 +14,9 @@ var header = `<div style="text-align: center; color: #0192DA; font-family: Verda
   </div>
 </div>
 </div>
-<div>
+<div style="background: #AAE8EE; border-style: solid; border-color: #254871; border-width: 0.2em 0em">
 <ul style="text-align: center">
-  <li style="display: inline"><a>Home</a></li>&nbsp;&nbsp;&nbsp;
+  <li style="display: inline"><a href="index.html">Home</a></li>&nbsp;&nbsp;&nbsp;
   <li style="display: inline"><a>Rules</a></li>&nbsp;&nbsp;&nbsp;
   <li style="display: inline"><a>Objects</a></li>&nbsp;&nbsp;&nbsp;
   <li style="display: inline"><a>Census</a></li>&nbsp;&nbsp;&nbsp;
